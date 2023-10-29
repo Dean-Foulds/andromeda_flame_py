4c574153efe4759f3652c32a1c2fa56a23ba5de7
Dean <dean@MSI.broadband>
2023-10-29 18:31:01 +0000

    postgres added

--- andromeda_flame/andromeda_flame/settings.py
+++ andromeda_flame/andromeda_flame/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'andromeda_flame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flame',
+        'USER': 'dean',
+        'PASSWORD': 'password',
+    }}
 
 
 # Password validation
