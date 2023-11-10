b61bdb5194e88c9ca8e2229425b0c586e9911f9e
Dean <dean@MSI.broadband>
2023-11-10 12:38:09 +0000

    Adjust buildpacks on Heroku

--- andromeda_flame/settings.py
+++ andromeda_flame/settings.py
@@ -79,12 +79,12 @@ TEMPLATES = [
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'flame',
-        'USER': 'dean',
-        'PASSWORD': '1234',
-    }}
+    "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+    ),
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
